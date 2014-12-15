1c4ee69f6af2e2b7b7d78c8bdbca3c8fc48e6a6f
Steve Weber <steverweber@gmail.com>
2014-12-15 14:09:54 -0500

    Update ghtogl.py
    migrated version to arg... then removed bad line
    https://github.com/steverweber/ghtogl/issues/11

--- ghtogl.py
+++ ghtogl.py
@@ -265,9 +265,7 @@ def main(args):
 
 if __name__ == '__main__':
   import argparse
-  
-  add_argument(..., action='version', version="N", ...)
-  
+
   parser = argparse.ArgumentParser(description='Migrate issues, comments, labels, and milestones from github project to gitlab project')
   parser.add_argument('--version', action='version', default=argparse.SUPPRESS, version='%(prog)s 1.1')
   parser.add_argument('--github_api', type=str, 
